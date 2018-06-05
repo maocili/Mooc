14151f7531da4a9bbe8bb4dde3c719b15f18f181
maocili <maocili@163.com>
2018-06-05 17:49:11 +0800

    存在遗漏问题

---
+++ main.py
@@ -0,0 +1,7 @@
+import  Login
+print('稳定遗漏几题，自己加上')
+acc = input('输入用户名密码，以空格区分'+'\n').split()
+head =  Login.requests_interface.login(acc[0],acc[1])
+Login.requests_interface.start('xxdgaeiojkdp4a39rqtp1q')
+
+# Begin.begin_interface.get_allId()
