6e0d2ca041d931b420fab43f0a77af943f84c78a
maocili <37239687+maocili@users.noreply.github.com>
2018-06-11 11:51:50 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,32 @@
-import requests
-import json
+import os
+import Login
+from time import sleep
+from tkinter import *
+class DirList(object):
+    def __init__(self, initdir=None):
+        self.top = Tk()
+        self.label = Label(self.top,text='Directory Lister v1.2')
+        self.label1 = Label(self.top,text='账号')
+        self.textbox1 = Text(self.top, width=80, height=1)
+        self.label2 = Label(self.top, text='密码')
+        self.textbox2 = Text(self.top, width=80, height=1)
+        self.button = Button(self.top,text='开始刷题',command=self.login)
+        self.label.pack()
+        self.label1.pack()
+        self.textbox1.pack()
+        self.label2.pack()
+        self.textbox2.pack()
+        self.button.pack()
 
-DATA={
-    'userName': '',
-    'password':''
-}
-head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36'}
+    def login(self,ev=None):
+        print(self.textbox1.get(0))
+        # Login.requests_interface.login()
+        # Login.requests_interface.start('xxdgaeiojkdp4a39rqtp1q')
 
+
+def main():
+    d = DirList(os.curdir)
+    mainloop()
+
+if __name__ == '__main__':
+    main()
